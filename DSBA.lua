4b11ba2996011a3d66cc9e0a6c57e084326064f5
zenoxthecutie <110969138+ZenoxLebg@users.noreply.github.com>
2023-06-13 04:02:09 +0200

    Update DSBA.lua
    Actually added a TweenService function instead of "TeleportToCFrame" bc that's retarded it'll just get me kicked.

--- DSBA.lua
+++ DSBA.lua
@@ -4,13 +4,14 @@ local humanoid = character:WaitForChild("Humanoid")
 local rootPart = character:WaitForChild("HumanoidRootPart")
 
 local platformHeight = 2 
-local platformSize = Vector3.new(4, 0.2, 4) 
+local platformSize = Vector3.new(4, 0.2, 4) -- u can ajust even tho its alright like this
 
 local createPlatform = true 
 
 local platform 
 local teleporting = false 
 local teleported = false 
+
 
 local function createPlatform()
     if platform then
@@ -28,6 +29,7 @@ local function createPlatform()
     end
 end
 
+
 local function updatePlatformPosition()
     if platform then
         local offset = Vector3.new(0, platformHeight + (rootPart.Size.Y / 2), 0)
@@ -43,11 +45,11 @@ createPlatform()
 
 local function teleportToCFrame(cframe, speed)
     if platform then
-        -- Create a new tween
-        local tweenInfo = TweenInfo.new(speed or 1, Enum.EasingStyle.Quad)
-        local tween = game:GetService("TweenService"):Create(rootPart, tweenInfo, {CFrame = cframe})
+        local targetCFrame = platform.CFrame * cframe
         
-        -- Set up the completion event for the tween
+        local tweenInfo = TweenInfo.new(speed or 1, Enum.EasingStyle.Quad)
+        local tween = game:GetService("TweenService"):Create(rootPart, tweenInfo, {CFrame = targetCFrame})
+        
         local connection
         connection = tween.Completed:Connect(function()
             connection:Disconnect() 
@@ -63,8 +65,20 @@ local function teleportToCFrame(cframe, speed)
         teleporting = true
         teleported = false
         print("TweenTP initiated")
-        -- Play the tween
         tween:Play()
+    end
+end
+
+local function instantTeleportToCFrame(cframe)
+    if platform then
+        platform:Destroy()
+        platform = nil
+        print("Platform Removed")
+        
+        rootPart.CFrame = cframe
+        
+        teleporting = false
+        teleported = true
     end
 end
 
@@ -73,14 +87,11 @@ local function checkPlayerDistance()
     local distance = (targetCFrame.Position - rootPart.Position).Magnitude
     if distance <= 300 and not teleporting and not teleported then
         print("300 studs reached")
-        teleportToCFrame(targetCFrame, 30) --so the closer it gets from 0 the faster it is, below 30 it gets risky
+        instantTeleportToCFrame(targetCFrame)
         print("Instant teleportation to target")
     elseif distance > 300 and teleported then
         teleported = false
     end
 end
 
-
 game:GetService("RunService").RenderStepped:Connect(checkPlayerDistance)
-
-teleportToCFrame(CFrame.new(-2844.87524, 1121.98523, -1008.09894, -0.707116485, -8.28877091e-06, -0.70709753, 3.40334955e-05, 1, -4.57502902e-05, 0.70709753, -5.64306974e-05, -0.707116485), 30)
